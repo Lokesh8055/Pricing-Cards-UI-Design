8ae210fef726bed4fdcda9ee8bfd335cf28b2e4d
Lokesh <lokeshdude7@hotmail.com>
2022-01-03 20:33:02 +0530

    Pricing card UI design

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Open Sans"],
+      },
+      colors: {
+        lightGrey: "#F4F0F3",
+        lightSilver: "#F8FAFE",
+        darkBlue: "#433F63",
+        darkPink: "#B577F6",
+        darkYellow: "#FFCC6D",
+      },
+    },
+  },
+  plugins: [],
+};
